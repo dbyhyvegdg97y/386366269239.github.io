deedfa68c297362b7f99bc5cfc0b41846f37f3fe
dbyhyvegdg97y <111529040+dbyhyvegdg97y@users.noreply.github.com>
2022-08-18 20:03:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 
   
     var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
+    apiKey: "AIzaSyDRK86uZm4dVIQC1Kg6vAxmXqS7Ua-Pu6I",
+    authDomain: "project-614a7.firebaseapp.com",
+    databaseURL: "https://project-614a7-default-rtdb.firebaseio.com",
+    storageBucket: "project-614a7.appspot.com",
+    messagingSenderId: "908834257030",
+    appId: "1:908834257030:web:088adf659c5fb06f3ab907",
   }; 
  
 ////Do Not Edit Below This
